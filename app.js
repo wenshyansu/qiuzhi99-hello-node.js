a3bf39c1304127cbf87f99244bcbbdc547423c32
Rock SU <wenshyansu@yahoo.com.tw>
2019-11-06 23:33:20 +0800

    clearInterval

--- app.js
+++ app.js
@@ -14,7 +14,10 @@
 
 var time = 0;
 
-setInterval(() => {
+var timer = setInterval(() => {
     time += 2;
     console.log(time + " seconds have passed");
+    if (time > 5) {
+        clearInterval(timer);
+    }
 }, 2000);
