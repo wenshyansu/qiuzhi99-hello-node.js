87e9dc3a195e984ec8acbc7d5b10d96714cdaa4f
Rock SU <wenshyansu@yahoo.com.tw>
2019-11-06 23:33:47 +0800

    __dirname, __filename

--- app.js
+++ app.js
@@ -12,12 +12,17 @@
 //     console.log("2 seconds have passed");
 // }, 2000);
 
-var time = 0;
+// var time = 0;
 
-var timer = setInterval(() => {
-    time += 2;
-    console.log(time + " seconds have passed");
-    if (time > 5) {
-        clearInterval(timer);
-    }
-}, 2000);
+// var timer = setInterval(() => {
+//     time += 2;
+//     console.log(time + " seconds have passed");
+//     if (time > 5) {
+//         clearInterval(timer);
+//     }
+// }, 2000);
+
+console.log(__dirname);
+console.log(__filename);
+
+// require exports
