bc4a0fb25c324494a70804a5a92912a12a3b5ef7
Rock SU <wenshyansu@yahoo.com.tw>
2019-11-06 23:32:49 +0800

    edit setInterval, var time

--- app.js
+++ app.js
@@ -8,6 +8,13 @@
 //     console.log("3 seconds have passed");
 // }, 3000);
 
+// setInterval(() => {
+//     console.log("2 seconds have passed");
+// }, 2000);
+
+var time = 0;
+
 setInterval(() => {
-    console.log("2 seconds have passed");
+    time += 2;
+    console.log(time + " seconds have passed");
 }, 2000);
